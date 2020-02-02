4683e9316a88d8ac8bebd7d6330b1f32632f2af0
Visweshwar Ganesh <vganesh@paychex.om>
2020-02-02 09:54:36 -0500

    Changing Signature

---
+++ src/main/java/com/paychex/oidc/client/Application.java
@@ -0,0 +1,33 @@
+/*
+ * # Copyright 2020 Paychex, Inc.
+ * # Licensed pursuant to the terms of the Apache License, Version 2.0 (the "License");
+ * # your use of the Work is subject to the terms and conditions of the License.
+ * # You may obtain a copy of the License at
+ * #
+ * # http://www.apache.org/licenses/LICENSE-2.0
+ * #
+ * # Disclaimer of Warranty. Unless required by applicable law or agreed to in writing, Licensor
+ * # provides the Work (and each Contributor provides its Contributions) on an "AS IS" BASIS,
+ * # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including,
+ * # without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ * # MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. You are solely responsible
+ * # for determining the appropriateness of using or redistributing the Work and assume
+ * # any risks associated with your exercise of permissions under this License.
+ */
+package com.paychex.oidc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+	Author: Visweshwar Ganesh
+	created on 2/2/20 8:27 AM
+*/
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
